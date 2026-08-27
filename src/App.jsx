627b33d8522c4849caefd49d7060bc246d12f5f1
saiful-aj18 <alexjamson118@gmail.com>
2026-08-27 21:24:56 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+//import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-slate-950 flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-white">
+        Product Explorer
+      </h1>
+    </div>
+  )
+}
+
+export default App
